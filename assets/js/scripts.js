c53b6f3af747be9f85ecbea9477e05788da36d3d
Johan Núñez <96917555+FE1T0R@users.noreply.github.com>
2024-09-18 20:46:19 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,7 +11,7 @@ function getCharacters() {
 
         success: function (data) {
             $("#mycounter").attr("style","color: blue");
-            $("#mycounter").attr("data-purecounter-end",data.info.pages);
+            $("#mycounter").attr("data-purecounter-end",data.info.count);
             var characters = data;
             $.each(characters.results, function (index, character) {
                 $("#divAllChar").append(uniqueCharacter(character));
